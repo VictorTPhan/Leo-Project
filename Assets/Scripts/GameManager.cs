211bd3f511bbf3e1189f1a24b401ec9fc9f60260
Leowwithw <theguy_leo@outlook.com>
2023-02-25 11:25:26 +0800

    Added All Initial Choices
    Added choices to all loans and investments, and added checks for opening 1 menu at a time.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -24,6 +24,7 @@ public class GameManager : MonoBehaviour
 
     //stores the pop ups
     public GameObject loanPopUp;
+    public GameObject investmentPopUp;
     public static GameObject warnPopup;
 
 
@@ -39,6 +40,7 @@ public class GameManager : MonoBehaviour
         warnPopup = GameObject.Find("WarnPopup");
         warnPopup.SetActive(false);
         loanPopUp.SetActive(false);
+        investmentPopUp.SetActive(false);
         loanChoice = 0;
         investChoice = 0;
     }
@@ -93,60 +95,155 @@ public class GameManager : MonoBehaviour
         //choice 2 -medium money
         if (loanChoice == 2)
         {
-
+            randFactor = Random.Range(-10,11); //picks random int from between -10 and 10
+            moneyToRecieve = (currentFund/2) + ( (currentFund/25) * randFactor);
+            quarterlyCostToIncrease = moneyToRecieve/10;
         }
         //choice 3 -high money
         if (loanChoice == 3)
         {
-
+            randFactor = Random.Range(-5,6); //picks random int from between -10 and 10
+            moneyToRecieve = (currentFund/2) + ( (currentFund/5) * randFactor);
+            quarterlyCostToIncrease = moneyToRecieve/4;
         }
         currentFund += moneyToRecieve;
         currentLost += quarterlyCostToIncrease;
+
+        Debug.Log("Player made: " + moneyToRecieve + "; Player quarter cost increase: " + quarterlyCostToIncrease);
 
         //turns on loan cooldown
         canChooseLoan = false;
     }
     //Handles the math for the amount of money gained/lossed.
-    public int CalculatingInvestment()
+    public void CalculatingInvestment()
     {
         int randFactor = 0; //random factor
         int moneyToRecieve = 0;
         int moneyToInvest = 0;
         int randChoice = 0;
+        int baseValue = 50; //value that all investments use and scales overtime with turn count.
+
+        baseValue = 50 + turnCount * (Random.Range(1, 7)) + (currentLost/150); //value is modified by turnCount * + or - 5%
 
         //spends players money for that month, but has change to make a return on invetsment (ROI)
         if (investChoice == 1)
         {
             randChoice = Random.Range(1,101); //determines at random which of the choices appear
-            if (randChoice <= 90) //%90 of the time
+            if (randChoice <= 90) //90% of the time
             {
-            //choice 1.1 - low money invested, low returns, completely safe
-
-            //MATH GOES HERE    
+            //choice 1.1 - low money invested, low returns, completely safe (100% returns)
+            moneyToInvest = baseValue / 2;
+            moneyToRecieve = baseValue * Random.Range(1,4);
 
             }
-            else
+            else //10% of the time
             {
-            //choice 1.2 - low money invested, modest returns, mostly safe
-
+            //choice 1.2 - low money invested, modest returns, mostly safe (90% returns)
+            moneyToInvest = baseValue / 2;
+            randFactor = Random.Range(1,101);
+                if (randFactor <= 90) {
+                    moneyToRecieve = baseValue * Random.Range(3,6);
+                }
+                else {
+                    moneyToRecieve = 0;
+                }
             }
         }
 
         //choice 2
-
+        if (investChoice == 2)
+        {
+            moneyToInvest = baseValue * 15;
+            randFactor = Random.Range(1,101);
+            if (randFactor <= 30) // 30% of the time, 
+            {
+                moneyToRecieve = 0;
+            }
+            else if (31 <= randFactor && randFactor <= 80) // 50% of the time, 
+            {
+                moneyToRecieve = baseValue * (15 + Random.Range(-5,6));
+            }
+            else // 20% of the time
+            {
+                 moneyToRecieve = baseValue * (19 + Random.Range(1,12));
+            }
+            
+        }
         //choice 3
-        return 0;
+        if (investChoice == 3)
+        {
+            moneyToInvest = baseValue * 50;
+            randFactor = Random.Range(1,101);
+            if (randFactor <= 60) // 60% of the time, 
+            {
+                moneyToRecieve = 0;
+            }
+            else if (61 <= randFactor && randFactor <= 85) // 25% of the time, 
+            {
+                moneyToRecieve = baseValue * (50 + Random.Range(-10,11));
+            }
+            else // 15% of the time
+            {
+                 moneyToRecieve = baseValue * (65 + Random.Range(1,16));
+            }
+
+        }
+
+        currentFund -= moneyToInvest;
+        currentFund += moneyToRecieve;
+
+        Debug.Log("Player spent: " + moneyToInvest + "; Player recieved: " + moneyToRecieve); 
     }
 
-    //method that reveals the loan pop up menu
+    //methods that reveals the pop up menus
     public void revealLoanMenu()
-    {  
+    {
+        if (canChooseLoan == false)
+        {
+            StartCoroutine(Popup(3.0f, "Can't Choose loan yet, please make an Investment."));
+            Debug.Log("Error: Can't take another loan");
+            loanChoice = 0;
+        }
+        else if (investmentPopUp.activeSelf == true)
+        {
+            StartCoroutine(Popup(2.0f, "Please close the Investment menu first"));
+        }  
+        else{
         loanPopUp.SetActive(true);
+        }
     }
 
     public void closeLoanMenu()
     {
+        if (investmentPopUp.activeSelf == true)
+        {
+            StartCoroutine(Popup(2.0f, "Please close the Investment menu first"));
+        }
+        else{
         loanPopUp.SetActive(false);
+        }
+    }
+
+     public void revealInvestmentMenu()
+    {  
+        if (loanPopUp.activeSelf == true)
+        {
+            StartCoroutine(Popup(2.0f, "Please close the Loan menu first"));
+        }  
+        else{
+        investmentPopUp.SetActive(true);
+        }
+    }
+
+    public void closeInvestmentMenu()
+    {
+        if (loanPopUp.activeSelf == true)
+        {
+            StartCoroutine(Popup(2.0f, "Please close the Loan menu first"));
+        }  
+        else{
+        investmentPopUp.SetActive(false);
+        }
     }
 
     /*
@@ -154,20 +251,47 @@ public class GameManager : MonoBehaviour
     */
     public void ChooseOptionOne()
     {
+        if (loanPopUp.activeSelf)
+        {
         loanChoice = 1;
         ChoosenLoans();
+        }
+
+        else
+        {
+        investChoice = 1;
+        ChoosenInvest();
+        }
     }
     public void ChooseOptionTwo()
     {
+        if (loanPopUp.activeSelf)
+        {
         loanChoice = 2;
         ChoosenLoans();
+        }
+
+        else
+        {
+        investChoice = 2;
+        ChoosenInvest();
+        }
     }
     public void ChooseOptionThree()
     {
+        if (loanPopUp.activeSelf)
+        {
         loanChoice = 3;
         ChoosenLoans();
-    }
-    
+        }
+
+        else
+        {
+        investChoice = 3;
+        ChoosenInvest();
+        }
+
+    }    
 
     //a method advances the turn when called
     public void AdvanceTurn()
@@ -183,6 +307,7 @@ public class GameManager : MonoBehaviour
             if (playerChoosenInvest)
             {
                 Debug.Log("Player choose to make an investment.");
+                CalculatingInvestment();
             }
 
         playerChoosenInvest = false;
