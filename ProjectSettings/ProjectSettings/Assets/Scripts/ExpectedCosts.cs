c974d63124c190cbb713735dfde31c337927192a
Leowwithw <theguy_leo@outlook.com>
2023-04-05 10:17:50 +0800

    april 4th
    Implemented loans and investments, and made preparations for win/lose conditions and screens.

---
+++ ProjectSettings/ProjectSettings/Assets/Scripts/ExpectedCosts.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class ExpectedCosts : MonoBehaviour
+{
+    public TextMeshProUGUI loanQuaterlyCosts;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        //loanQuaterlyCosts.text = string.Format("Expected Losses : -$" + );
+    }
+}
